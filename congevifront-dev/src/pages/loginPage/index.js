25d38dd2c3caafa4dfc7db97aad984d31925174b
Gabriel Pinheiro <gabrielpinheiroomc@gmail.com>
2024-04-30 02:53:34 -0300

    frontend2 loginpapge

--- congevifront-dev/src/pages/loginPage/index.js
+++ congevifront-dev/src/pages/loginPage/index.js
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
 import styles from './loginstyles.module.css';
+import logo from './img/logo-removebg-preview.png'; // Importe a imagem aqui
 import AuthContext from '../../context/AuthContext';
 
 const LoginPage = () => {
   const { loginUser, user } = useContext(AuthContext);
 
+
   return (
     <div className={styles.corpo}>
+      <img className={styles.imagem} src={logo} alt="" />
+
       <form onSubmit={loginUser} className={styles.loginContainer}>
-        <h2>Login</h2>
+        <h2 className={styles.login}>Login</h2>
         {user ? <p>Você está logado!</p> : ''}
         <div className={styles.campos}>
           <div className={styles.inputField}>
